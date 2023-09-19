2609a3d4b7b5128559ffe145b769d6fb7ccec38c
dajo502 <dajo502@PC30447>
2023-09-19 11:11:38 +0200

    Add project files.

---
+++ SUT23-230919/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SUT23_230919
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
